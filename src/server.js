ee9769554c8bb7579ecc7f0f8178c44d1841c4fb
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-06-20 19:30:26 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -1,8 +1,60 @@
 // install and import express
-const express = () => {};
+
+// const express = () => {};
+const express = require("express")
 let app = express();
-
+const User = require('./assets/user.json')
 // Code here
+app.get('/',async(req,res)=>{
+    try {
+        const user = await User.find().lean().exec()
+        return res.status(200).send(user)
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+})
 
+app.post('/users',async(req,res)=>{
+    try {
+        const user = await User.create(req.body)
+        return res.status(201).send(user)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+app.get('/users/:id',async(req,res)=>{
+    try {
+        const user = await User.find(req.params.id).lean().exec()
+        return res.status(200).send(user)
+    } catch (error) {
+        return res.status(400).send(error)
+    }
+})
+
+app.get('/users/:id',async(req,res)=>{
+    try {
+        const user = await User.find(req.params.id).lean().exec()
+        return res.status(201).send(user)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+app.get('/users',async(req,res)=>{
+    try {
+        const user = await User.find().lean().exec()
+        return res.status(201).send(user)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+// app.listen('8080',async()=>{
+//     try {
+//         console.log('listening On port 8000')
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })
 // Note: Do not remove this export statement
+
 module.exports = app;
